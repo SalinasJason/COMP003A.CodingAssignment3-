0f6b3943a2abdbf17b7fffcf42a8affb7490c99d
SalinasJason <jasonsalinas770@gmail.com>
2025-01-29 21:35:35 -0800

    Asked the user to "Enter your monthly income" and added do while loop and a display menu

--- COMP003A.CodingAssignment3-/Program.cs
+++ COMP003A.CodingAssignment3-/Program.cs
@@ -9,6 +9,22 @@ namespace COMP003A.CodingAssignment3_
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Budget Management Tool!");
+
+            Console.Write("\nEnter your monthly income: ");
+            double num = double.Parse(Console.ReadLine());
+
+            int choice;
+
+            do
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Add an Expense");
+                Console.WriteLine("2. View Expenses and Budget");
+                Console.WriteLine("3. Remove an Expense");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter you choice: ");
+                choice = int.Parse(Console.ReadLine());
+            } while (choice != 4);
         }
     }
 }
