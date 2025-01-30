44cc2213c1f9689ce14f55396979bdae274ab9f2
SalinasJason <jasonsalinas770@gmail.com>
2025-01-29 21:06:44 -0800

    Added top comments and greeting

--- COMP003A.CodingAssignment3-/Program.cs
+++ COMP003A.CodingAssignment3-/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace COMP003A.CodingAssignment3_
+﻿// Author: Jason Salinas
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Budget management application demonstrating control flow.
+namespace COMP003A.CodingAssignment3_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Budget Management Tool!");
         }
     }
 }
