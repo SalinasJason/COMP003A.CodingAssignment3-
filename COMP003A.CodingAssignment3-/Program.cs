5f811207337333d10a7eee8a1ea29279d28d6307
SalinasJason <jasonsalinas770@gmail.com>
2025-01-29 21:05:06 -0800

    Add project files.

---
+++ COMP003A.CodingAssignment3-/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.CodingAssignment3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
